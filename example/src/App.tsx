cf9c6fad88c848757ca2f1776932febb2fd33a6e
Julius Kiano <jgkiano@gmail.com>
2020-08-30 22:44:30 +0300

    use OkHiLocationManager

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,21 +1,51 @@
 import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import ReactNativeOkcollect from '@okhi/react-native-okcollect';
-import { WebView } from 'react-native-webview';
+import OkHiLocationManager from '@okhi/react-native-okcollect';
+import { OkHiAuth, OkHiContext } from '@okhi/react-native-core';
+import { branchId, clientKey, mode } from './secret.json';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const context = new OkHiContext({
+    mode,
+    app: {
+      name: 'My Awesome App',
+      build: 1,
+      version: '1.0.0',
+    },
+  });
 
-  React.useEffect(() => {
-    ReactNativeOkcollect.multiply(3, 7).then(setResult);
-  }, []);
+  const auth = OkHiAuth.withContext(
+    {
+      branchId,
+      clientKey,
+    },
+    context
+  );
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
-      <WebView
-        source={{ uri: 'https://infinite.red' }}
-        style={{ marginTop: 20, height: 300, width: 300 }}
+      <OkHiLocationManager
+        auth={auth}
+        launch={true}
+        user={{
+          phone: '+254700110590',
+        }}
+        onError={console.log}
+        onSuccess={console.log}
+        onCloseRequest={() => console.log('Close me')}
+        theme={{
+          appBar: {
+            backgroundColor: '#333',
+            logo:
+              'https://gblobscdn.gitbook.com/spaces%2F-LZKFOZdghaDd109RYxt%2Favatar.png?alt=media',
+          },
+          colors: {
+            primary: '#333',
+          },
+        }}
+        config={{ appBar: { visible: false }, streetView: false }}
+        loader={<Text>Loading..</Text>}
+        // style={{ padding: 30, backgroundColor: 'red' }}
       />
     </View>
   );
