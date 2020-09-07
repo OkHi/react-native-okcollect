347ff573fa50b79c9f4d819f264528b23508f792
Julius Kiano <jgkiano@gmail.com>
2020-09-07 22:03:56 +0300

    added tsdocs comments

--- src/OkHiLocationManagerCore.ts
+++ src/OkHiLocationManagerCore.ts
@@ -1,5 +1,8 @@
 import { OkHiCore, OkHiAuth } from '@okhi/react-native-core';
 
+/**
+ * @ignore
+ */
 export class OkHiLocationManagerCore extends OkHiCore {
   constructor(auth: OkHiAuth) {
     super(auth);
