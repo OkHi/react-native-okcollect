b699e2811a6ca7096c4b5e2873375a281944288f
Julius Kiano <jgkiano@gmail.com>
2021-06-03 11:18:29 +0300

    added method for retriving ApplicationConfiguration

--- src/OkHiLocationManagerCore.ts
+++ src/OkHiLocationManagerCore.ts
@@ -1,13 +1,13 @@
-import { OkHiCore, OkHiAuth } from '@okhi/react-native-core';
+import { OkHiCore } from '@okhi/react-native-core';
 
 /**
  * @ignore
  */
 export class OkHiLocationManagerCore extends OkHiCore {
-  constructor(auth: OkHiAuth) {
-    super(auth);
-  }
   getBearerToken(phone: string) {
     return this.anonymousSignInWithPhoneNumber(phone, ['address']);
+  }
+  getConfiguration() {
+    return this.getApplicationConfiguration();
   }
 }
